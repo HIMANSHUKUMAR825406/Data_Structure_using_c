c27adce664c7f37b5c412edb00a6d6093f8b339d
Himanshu Kumar Saw <129617476+himanshu0ic7@users.noreply.github.com>
2023-10-17 22:33:52 +0530

    Update Insertion_sort.c

--- Sorting_techniques/Insertion_sort.c
+++ Sorting_techniques/Insertion_sort.c
@@ -13,7 +13,9 @@ int main(void) {
   for(int i=0;i<n;i++) scanf("%d",&arr[i]);
   for(int i=1;i<n;i++){
     for(int j=i;j>0;j--){
-      if(arr[j-1]>arr[j]) swap(&arr[j],&arr[j-1]);
+      if(arr[j-1]<arr[j]) break;
+      else swap(&arr[j],&arr[j-1]);
+      //if(arr[j-1]>arr[j]) swap(&arr[j],&arr[j-1]); I have replaced this line with above two lines of code
     }
   }
   printf("Element after sorting: \n");
